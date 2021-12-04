92acda155cf8507a9e6bf3d0ae475666553ed570
Gerdin Ventura Croussett <gerdinventuraedu@gmail.com>
2021-12-03 23:28:05 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gerdin Ventura on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
