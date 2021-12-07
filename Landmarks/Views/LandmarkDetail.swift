e6d1638b5d593fd7543cba83f361c4cf60837913
Gerdin Ventura Croussett <gerdinventuraedu@gmail.com>
2021-12-07 03:45:51 -0500

    Created navigation view. Displays static data (for now)

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Gerdin Ventura on 12/7/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+//                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,16 +9,24 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        LazyVStack {
-            ForEach(landmarks) { landmark in
-                LandmarkRow(landmark: landmark)
-            }
-        }
         
-//        List(landmarks, id: \.id) { landmark in
-//                    LandmarkRow(landmark: landmark)
-//        }
-
+        NavigationView {
+            LazyVStack {
+                ForEach(landmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail()
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                        Text(">")
+                            .foregroundColor(.secondary)
+                            .padding(.trailing, 10)
+                    }
+                }.foregroundColor(.primary)
+            }
+            .padding(.top, 100)
+            .padding(.leading, 5)
+            .navigationTitle("Landmarks")
+        }
     }
 }
 
